ecb16fb2f12fafd23d29abf1087109a30275dd18
Philip Kocanda <philip@kocanda.nl>
2017-09-17 11:55:40 +0200

    test

--- index.js
+++ index.js
@@ -1,74 +1,20 @@
 'use strict';
 
-const request = require('request');
-const url = require('url');
-
-let Service, Characteristic;
+let Accessory, Service, Characteristic;
 
 module.exports = function (homebridge) {
+  Accessory = homebridge.hap.Accessory;
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("generic-lock-plugin", "MyGenericLock", myLock);
+
+  const exportedTypes = {
+    Accessory: Accessory,
+    Service: Service,
+    Characteristic: Characteristic
+  };
+
+  const LockAccessory = require('./lib/lock-accessory.js')(exportedTypes);
+
+  homebridge.registerAccessory("generic-lock-plugin", "MyGenericLock", LockAccessory);
 };
 
-function myLock(log, config) {
-  this.log = log;
-  this.postUrl = url.parse(config['postUrl']);
-  this.name = config['name'];
-  this.lockState = Characteristic.LockCurrentState.SECURED;
-}
-
-myLock.prototype.getServices = function() {
-  let informationService = new Service.AccessoryInformation();
-  informationService
-    .setCharacteristic(Characteristic.Manufacturer, "Acme Corp.")
-    .setCharacteristic(Characteristic.Model, "GenericLock v1")
-    .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
-
-  let lockService = new Service.LockMechanism(this.name);
-
-  lockService
-    .getCharacteristic(Characteristic.LockCurrentState)
-      .on('get', this.getLockState.bind(this))
-
-  lockService
-    .getCharacteristic(Characteristic.LockTargetState)
-      .on('get', this.getLockState.bind(this))
-      .on('set', this.setLockState.bind(this));
-
-  this.informationService = informationService;
-  this.lockService = lockService;
-  return [informationService, lockService];
-};
-
-myLock.prototype.getLockState = function(callback) {
-  return callback(null, this.lockState);
-};
-
-myLock.prototype.setLockState = function(targetState, callback) {
-  // optimistic
-  this.lockState = targetState;
-
-  request({
-    url: this.postUrl,
-    body: JSON.stringify({'targetState': targetState}),
-    method: 'POST',
-    headers: {'Content-type': 'application/json'}
-  }, this.handlePostResponse.bind(this));
-};
-
-myLock.prototype.handlePostResponse = function(error, response) {
-  if (error) {
-    if (response) {
-      this.log('STATUS: ' + response.statusCode);
-    }
-    this.log(error.message);
-    return callback(error);
-  }
-
-  // This lock only unlocks temporarily, so change the state back to locked after an interval.
-  setTimeout(function() {
-    this.lockState = Characteristic.LockCurrentState.SECURED;
-    callback(null, targetState);
-  }, 500);
-};
